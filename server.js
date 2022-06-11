285e0b6dc62ef01fe8253a66ea5ee513d1735f55
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2022-06-11 11:34:48 +0430

    I made a server.js and socket.io

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+require("dotenv").config();
+const http = require("http");
+const socketIo = require("socket.io");
+
+//making app module
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+//middle wares
+app.use(express.static("./public"));
+
+io.on("connection", socket => {
+  console.log("Socket connected to server");
+  socket.emit("message","welcome to appCord Chat");
+  socket.broadcast.emit("message", "mahdi joined to chat");
+    socket.on("disconnect",() =>{
+        io.emit("message","Some user disconnected");
+    })
+
+});
+
+
+
+const port = process.env.PORT || 3000;
+server.listen(port, console.log("server started in port 3000"));
